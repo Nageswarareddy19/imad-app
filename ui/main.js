7760447606ed9ef7fe36758dbd14b55f6b9da31a
Nageswarareddy19 <30750395+Nageswarareddy19@users.noreply.github.com>
2017-09-17 12:16:40 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -4,5 +4,5 @@ element.innerHTML="NageswaraReddy";
 //move image
 var img=document.getElementById("madi");
 img.Onclick=function(){
-    img.style.marginLeft='100px';
+    img.style.marginLeft='1000px';
 };
